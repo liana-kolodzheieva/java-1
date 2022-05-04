228afb825d58e1a744de89c101e11877ca2f94f6
liana kolodgeeva <liana.kolodgeeva@gmail.com>
2022-05-04 15:39:47 +0100

    add  homewotk4

--- src/Java/Homework/homewotk4.java
+++ src/Java/Homework/homewotk4.java
@@ -6,48 +6,34 @@ public class homewotk4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //дано
-        System.out.println("write 1 ball: ");
-        double ball1 = Double.parseDouble(scanner.nextLine());
-        System.out.println("write 2 ball: ");
-        double ball2 = Double.parseDouble(scanner.nextLine());
-        System.out.println("write 3 ball: ");
-        double ball3 = Double.parseDouble(scanner.nextLine());
-        double d = 3;
+        System.out.println("write r ball1 1: ");
+        double r1 = Double.parseDouble(scanner.nextLine());
+        System.out.println("write r ball 2: ");
+        double r2 = Double.parseDouble(scanner.nextLine());
+        System.out.println("write r ball 3: ");
+        double r3 = Double.parseDouble(scanner.nextLine());
+        double degree = 3;
         double pi = Math.PI;
         // деля узнавания 3 шаров
-        double number = 4.0/3.0;
+        double number = 4.0 / 3.0;
         // узнаем 1 шар
-        //r3
-        double r3 = Math.pow(ball1, d);
-        //Pi*r3
-        double pir3 = pi * r3;
-        //последнее действие
-        double ball = number * pir3;
-
+        double masa1 = (double) (number * pi * Math.pow(r1, degree));
         //узнаем шар 2
-        // r3
-        double r = Math.pow(ball2, d);
-        //Pi*r3
-        double pir = pi * r;
-        //последнее действие
-        double bal2 = number * pir;
+        Double masa2 = (Double) (number * pi * Math.pow(r2, degree));
+
 
         //узнаем шар 3
-        //r3
-        double rз = Math.pow(ball3, d);
-        //Pi*r3
-        double pirз = pi * rз;
-        //последнее действие
-        double bal3 = number * pirз;
+        Double masa3 = (Double) (number * pi * Math.pow(r3, degree));
+
 
         //результат
-        System.out.println("first ball : " + ball);
-        System.out.println("second ball : " + ball2);
-        System.out.println("third ball : " + bal3);
-        double balls = (int) (ball + ball2 + bal3);
-        System.out.println(ball + "+" + ball2 + "+" + bal3 + "=" + balls);
+        System.out.println("first ball : " + masa1);
+        System.out.println("second ball : " + masa2);
+        System.out.println("third ball : " + masa3);
+        double balls = (double) (masa1 + masa2 + masa3);
+        System.out.println(masa1 + "+" + masa2 + "+" + masa3 + "=" + balls);
         System.out.println("snowman mass: " + balls);
-        }
     }
+}
 
 
